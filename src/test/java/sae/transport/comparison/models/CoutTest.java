29f467bf27473d6e5f78e02592e0c35e0a55f333
Yann Renard <yannrenard1025@gmail.com>
2026-05-01 18:09:10 +0200

    feat: add CoutTest class with unit tests for Cout model

---
+++ src/test/java/sae/transport/comparison/models/CoutTest.java
@@ -0,0 +1,39 @@
+package sae.transport.comparison.models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoutTest {
+
+    @BeforeEach
+    void setUp() {
+        Cout cout = new Cout(60.0, 80.0, 1.7);
+    }
+
+    @Test
+    void testGetValeurPrix() {
+        Cout cout = new Cout(60.0, 80.0, 1.7);
+        assertEquals(60.0, cout.getValeur(TypeCout.PRIX));
+    }
+
+    @Test
+    void testGetValeurTemps() {
+        Cout cout = new Cout(60.0, 80.0, 1.7);
+        assertEquals(80.0, cout.getValeur(TypeCout.TEMPS));
+    }
+
+    @Test
+    void testGetValeurCO2() {
+        Cout cout = new Cout(60.0, 80.0, 1.7);
+        assertEquals(1.7, cout.getValeur(TypeCout.CO2));
+    }
+
+    @Test
+    void testGetValeurZero() {
+        Cout cout = new Cout(0.0, 0.0, 0.0);
+        assertEquals(0.0, cout.getValeur(TypeCout.PRIX));
+    }
+}
