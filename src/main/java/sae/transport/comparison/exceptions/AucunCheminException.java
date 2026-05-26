0bd655d27e447e826dc185b3e3ed0d7625c83286
Yann Renard <yannrenard1025@gmail.com>
2026-05-26 22:30:08 +0200

    feat: add exports for exceptions and refine module access in module-info.java

---
+++ src/main/java/sae/transport/comparison/exceptions/AucunCheminException.java
@@ -0,0 +1,7 @@
+package sae.transport.comparison.exceptions;
+
+public class AucunCheminException extends Exception {
+    public AucunCheminException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/sae/transport/comparison/exceptions/DonneesInvalidesException.java
@@ -0,0 +1,7 @@
+package sae.transport.comparison.exceptions;
+
+public class DonneesInvalidesException extends Exception {
+    public DonneesInvalidesException(String message) {
+        super(message);
+    }
+}
